b3273190e222e0d219b0ff092ca9985ac629e80c
vm03 <vm03@yandex.ru>
2015-04-09 14:27:24 +0300

    Fix manual network selection
    Change-Id: I07d28e8d7c5212ab18595585468471ba557626ee

--- ril/telephony/java/com/android/internal/telephony/LgeRIL.java
+++ ril/telephony/java/com/android/internal/telephony/LgeRIL.java
@@ -16,7 +16,12 @@
 
 package com.android.internal.telephony;
 
+import static com.android.internal.telephony.RILConstants.*;
+
 import android.content.Context;
+import android.os.AsyncResult;
+import android.os.Message;
+
 
 public class LgeRIL extends RIL implements CommandsInterface {
 	public LgeRIL(Context context, int networkMode, int cdmaSubscription, Integer instanceId) {
@@ -24,4 +29,22 @@ public class LgeRIL extends RIL implements CommandsInterface {
 		
 		mQANElements = 5;
 	}
+
+	@Override
+	public void
+	setNetworkSelectionModeManual(String operatorNumeric, Message response) {
+		RILRequest rr
+			= RILRequest.obtain(RIL_REQUEST_SET_NETWORK_SELECTION_MANUAL,
+				response);
+
+		if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest)
+			+ " " + operatorNumeric);
+
+		rr.mParcel.writeInt(2);
+		rr.mParcel.writeString(operatorNumeric);
+		rr.mParcel.writeString("NOCHANGE");
+
+		send(rr);
+	}
+
 }
