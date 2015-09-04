95ef7e28975456d8c6be8d4e3430a39b0222adfd
vm03 <vm03@yandex.ru>
2015-09-04 11:10:03 +0300

    Playback : Use low_end google codecs and add msm8x10 codec capabilities
    *Though this wouldn't significantly help in normal usage but will help when external displays are used
    
    Change-Id: Ic0f9e1d12952b08c31f5e09cd98872a67eab0dca

--- msm8610.mk
+++ msm8610.mk
@@ -351,7 +351,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
 
 # Radio and Telephony
 PRODUCT_PROPERTY_OVERRIDES += \
